4b083656d2031836a3ec30c9a6226e623414e219
Chris Grimmett <chris@grimtech.net>
2020-11-01 14:48:24 -0800

    add cli test progress

--- test/cli.test.js
+++ test/cli.test.js
@@ -2,9 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const cliPath = path.join(__dirname, '..', 'p-data.js');
 const Promise = require('bluebird');
-const yargsParser = require('yargs-parser')
-const pify = require('pify')
-const glob = require('glob')
 const spawn = require('child_process').spawn
 const nock = require('nock')
 jest.mock('fs')
@@ -13,20 +10,21 @@ jest.mock('fs')
 nockBack = nock.back
 nockBack.fixtures = path.join(__dirname, '..', 'fixtures')
 
-
+const mockSetAbbr = [
+  {
+    "setAbbr": "ClariS",
+    "setUrl": "http://p-memories.com/card_product_list_page?field_title_nid=241831-ClariS&s_flg=on"
+  }
+]
+const mockFileStructureC = {
+  [path.join(__dirname, '..', 'data', 'setAbbrIndex.json')]: JSON.stringify(mockSetAbbr)
+}
 
 // greets https://github.com/kentcdodds/split-guide/blob/fb4b2a2ebc1fb8c3c010c2af1318861b8bb1bb13/src/bin/index.test.js
-function runCLIAndAssertFileOutput(args, cwd) {
-  // const {
-  //   exercisesDir = './exercises', exercisesFinalDir = './exercises-final'
-  // } = yargsParser(args)
+function runCLIAndAssertOutput(args, cwd) {
   return runCli(args, cwd)
     .then((stdout) => {
       expect(stdout).toMatchSnapshot()
-    })
-    .catch((e) => {
-      console.error('runCLI error')
-      console.error(e)
     })
 }
 
@@ -42,11 +40,13 @@ function runCli(args = '', cwd = process.cwd()) {
     let stderr = ''
     const command = path.join(__dirname, '..', 'p-data.js')
     const opts = {
-      cwd: cwd
+      cwd: cwd,
+      stdio: 'inherit'
     }
     const child = spawn(command, args.split(' '), opts)
 
     child.on('error', error => {
+      console.error('there was an error with the child_process')
       reject(error)
     })
 
@@ -59,11 +59,7 @@ function runCli(args = '', cwd = process.cwd()) {
     })
 
     child.on('close', () => {
-      if (stderr) {
-        reject(stderr)
-      } else {
-        resolve(stdout)
-      }
+      resolve(stdout)
     })
   })
 }
@@ -74,7 +70,7 @@ describe('p-data.js CLI', () => {
     it('should list all sets listed on p-memories.com', () => {
       return nockBack('cliList.1.json')
         .then(({ nockDone, context }) => {
-          return runCLIAndAssertFileOutput('list --type setNames --json', path.resolve(__dirname, '../'))
+          return runCLIAndAssertOutput('list --type setNames --json', path.resolve(__dirname, '../'))
             .then(() => {
               context.assertScopesFinished()
             })
@@ -83,69 +79,103 @@ describe('p-data.js CLI', () => {
     })
   })
   describe('index subcommand', () => {
-    it('should run without failure', () => {
-      return runCLIAndAssertFileOutput('index', path.resolve(__dirname, '../'))
-    })
     it('should create a setAbbrIndex.json file', () => {
-      require('fs').__setMock
-      return runCli('index', path.resolve(__dirname, '../'))
-        .then((stdout) => {
-
+      jest.spyOn(ripper, getSetUrls).mockImplementation(() => mockSetAbbr)
+      let expectedSetAbbrIndexPath = path.join(__dirname, '..', 'data', 'setAbbrIndex.json')
+      return nockBack('cli.index.2.json')
+        .then(({ nockDone, context }) => {
+          return runCLIAndAssertOutput('index', path.resolve(__dirname, '../'))
+            .then(() => {
+              expect(require('fs').__getWrittenFiles())
+                .toEqual(expect.arrayContaining([expectedSetAbbrIndexPath]))
+              context.assertScopesFinished()
+            })
+            .then(nockDone)
         })
-    })
+    }, 1000*30)
   });
   describe('rip subcommand', () => {
     it(
       'should make a Set suggestion if the Set abbreviation doesnt have an exact match',
       () => {
-        const parser = require('yargs')().command(require('../commands/rip'))
-        return new Promise((resolve, reject) => {
-          parser.parse('rip -s OREIMO', (err, argv, output) => {
-            if (err) reject(err)
-            resolve(output)
+        return nockBack('cli.rip.1.json')
+          .then(({ nockDone, context }) => {
+            const parser = require('yargs')().command(require('../commands/rip'))
+            return new Promise((resolve, reject) => {
+              parser.parse('rip -s OREIMO', (err, argv, output) => {
+                if (err) reject(err)
+                resolve(output)
+              })
+            }).then((output) => {
+              expect(output).toMatch(/Did you mean/)
+              context.assertScopesFinished()
+            })
+            .then(nockDone)
           })
-        }).then((output) => {
-          expect(output).toMatch(/Did you mean/)
-        });
-
       }
     )
     it('should rip a card given a URL to the card', () => {
-      return execFile(cliPath, ['node', './p-data.js', 'rip', '-u', 'http://p-memories.com/node/906300'])
-        .then((res) => {
-          expect(typeof res).toBe('string');
-          expect(res).toMatch(/涼風 青葉/)
-        });
+      return nockBack('cli.rip.2.json')
+        .then(({ nockDone, context }) => {
+          return runCLIAndAssertOutput('rip -u http://p-memories.com/node/906300')
+          .then((res) => {
+            expect(typeof res).toBe('string');
+            expect(res).toMatch(/涼風 青葉/)
+            context.assertScopesFinished()
+          })
+          .then(nockDone)
+        })
     })
     it(
       'should rip a card given the set abbreviation and card number',
       () => {
-        return execFile(cliPath, ['node', './p-data.js', 'rip', '-s', 'SG', '-n', '01-019'])
-          .then((res) => {
-            expect(typeof res).toBe('string');
-            expect(res).toMatch(/岡部 倫太郎/);
-          });
+        return nockBack('cli.rip.3.json')
+          .then(({ nockDone, context }) => {
+            return runCLIAndAssertOutput('rip -s SG -n 01-019')
+              .then((res) => {
+                expect(typeof res).toBe('string');
+                expect(res).toMatch(/岡部 倫太郎/);
+              });
+          })
       }
     )
     it('should rip an entire set given the set abbreviation', () => {
-      let program = cli.parse(['node', './p-data.js', 'rip', '--set', 'MMDK']);
-      expect(program).toBeDefined();
-      expect(program.set).toEqual('MMDK');
-
-      // .then((res) => {
-      //   assert.isString(res);
-      //   assert.match(res, /劇場版 魔法少女まどか☆マギカ/);
-      //   const
-      // })
+      return nockBack('cli.rip.4.json')
+        .then(({ nockDone, context }) => {
+          return runCLIAndAssertOutput('rip --set ClariS')
+            .then(() => {
+              const clarisPath = path.join(__dirname, '..', 'data', 'ClariS', 'P')
+              expect(fs.__getWrittenFiles()).toEqual(expect.arrayContaining([
+                path.join(clarisPath, 'ClariS_P-001.jpg'),
+                path.join(clarisPath, 'ClariS_P-001.json'),
+              ]))
+              context.assertScopesFinished()
+            })
+            .then(nockDone)
+          })
     })
-    xit('should have silent output when called with -q flag', function() {
-
+    it('should have silent output when called with -q flag', function() {
+      return nockBack('cli.rip.5.json')
+        .then(({ nockDone, context }) => {
+          return runCLIAndAssertOutput("rip -n \'HMK 01-001\' -q")
+            .then((stdout) => {
+              expect(stdout).toBeNull()
+              context.assertScopesFinished()
+            })
+            .then(nockDone)
+        })
     })
 
     it('should rip all incrementally with a throttle', () => {
-      let program = cli.parse(['node', './p-data.js', 'rip', '-a', '-i', '-t', '3']);
-      expect(program).toBeDefined();
-      console.log(program);
+      require('fs').__setMockFiles(mockFileStructureC)
+      return nockBack('cli.rip.6.json')
+        .then(({ nockDone, context }) => {
+          return runCLIAndAssertOutput('rip -a -i -t 3')
+          .then(() => {
+            conext.assertScopesFinished()
+          })
+          .then(nockDone)
+      })
     })
   })
 })
