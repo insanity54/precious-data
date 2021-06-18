7e22ba86e49a82b4eb0230e2d397d3da58274a9e
Chris Grimmett <chris@grimtech.net>
2021-06-17 19:46:01 -0700

    add sets to exports

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 
 const path = require('path');
 const globby = require('globby');
+const fs = require('fs');
 
 const exp = {
 	cards: collectCards(),
+	sets: collectSets(),
 	version: require('./package.json').version
 };
+
+function getCardImagePath(cardJsonPath) {
+	const directory = path.dirname(cardJsonPath);
+	const basename = path.basename(cardJsonPath, '.json');
+	return path.join(directory, `${basename}.jpg`);
+}
+
+function collectSets() {
+	const setAbbrIndex = require(path.join(__dirname, 'data', 'setAbbrIndex.json'));
+	return setAbbrIndex;
+}
 
 function collectCards() {
 	const cardPaths = globby.sync(path.join(__dirname, './data/**/*.json'));
@@ -14,7 +27,11 @@ function collectCards() {
 
 	for (cardPath of cardPaths) {
 		if (!cardPath.includes('setAbbrIndex.json')) {
-			const d = require(cardPath);
+			let d = require(cardPath);
+			// add imageData to the json
+			d['imagePath'] = getCardImagePath(cardPath)
+
+			// add card data to data object
 			data.push(d);
 		}
 	}
--- test/export.test.js
+++ test/export.test.js
@@ -3,9 +3,10 @@ const toBeType = require('jest-tobetype');
 expect.extend(toBeType)
 
 describe('precious-data exports', () => {
-  it('should export an object containg cards and version properties', () => {
+  it('should export an object containg cards, version, and sets properties', () => {
     expect(preciousData).toHaveProperty('version');
     expect(preciousData).toHaveProperty('cards');
+    expect(preciousData).toHaveProperty('sets');
   })
   it('should have a cards property that is an Array', () => {
     expect(preciousData.cards).toBeType('array');
@@ -13,13 +14,41 @@ describe('precious-data exports', () => {
   it('should have version property which is a string', () => {
     expect(preciousData.version).toBeType('string');
   })
+  it('should have a sets property which is an Array', () => {
+    expect(preciousData.sets).toBeType('array');
+  })
   it('should have a version property matching the package version', () => {
     const version = require('../package.json').version;
     expect(preciousData.version).toEqual(version);
+  })
+  it('should have a cards property which contains imagePath', () => {
+    expect(preciousData.cards[0]).toHaveProperty('imagePath')
+    expect(preciousData.cards[0].imagePath).toMatch(/\/data\/.*\.jpg/)
   })
   it('should have cards property which contains HMK 01-001', async () => {
     const cards = preciousData.cards
     const hmk = cards.find(c => c.number === '01-001' && c.setAbbr === 'HMK' )
     expect(hmk).toBeTruthy();
+  })
+  it('should have sets property which contains HMK, oreimo, and SSSS', async () => {
+    const sets = preciousData.sets
+    expect(sets).toContainEqual(
+      expect.objectContaining({
+        setAbbr: expect.stringMatching('HMK'),
+        setUrl: expect.stringMatching('p-memories.com'),
+      })
+    )
+    expect(sets).toContainEqual(
+      expect.objectContaining({
+        setAbbr: expect.stringMatching('oreimo'),
+        setUrl: expect.stringMatching('p-memories.com'),
+      })
+    )
+    expect(sets).toContainEqual(
+      expect.objectContaining({
+        setAbbr: expect.stringMatching('SSSS'),
+        setUrl: expect.stringMatching('p-memories.com'),
+      })
+    )
   })
 })
